b2cec9de0ab225b8103627dbd9e6cfa855d35e47
Jhon Vitor <jhonsantos.vs1@gmail.com>
2024-11-27 16:56:10 -0300

    created database

---
+++ tabela homenagem.sql
@@ -0,0 +1,8 @@
+﻿SELECT 
+nome_usuario,
+descricao_prece,
+nome_vitima
+
+FROM prece as p 
+INNER JOIN homenagem as h ON h.id_prece = p.id_prece
+INNER JOIN vitima as v ON v.id_vitima = h.id_vitima;
---
+++ tabela prece.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE prece(
+	id_prece INT PRIMARY KEY IDENTITY(1,1),
+	nome_usuario NVARCHAR(255) NOT NULL,
+	descricao_prece NVARCHAR(1000) NOT NULL
+);
