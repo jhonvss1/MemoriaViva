9fc695dbc33ae59b51e81275185a5db1641d8b36
Jhon Vitor <jhonsantos.vs1@gmail.com>
2024-11-27 11:39:00 -0300

    first version with html and css

---
+++ WebSite2/Default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void btnOrdemAleatoria_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnOrdemAlfabetica_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSalvarPrece_Click(object sender, EventArgs e) 
+    { 
+
+    }
+    protected void btnAdicionarVitimaModal_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnAcenderVela_Click(object sender, EventArgs e)
+    {
+
+    }
+}
